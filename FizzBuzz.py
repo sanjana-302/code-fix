31a90668034b0fcde579cb5b1af6c21ae58ee470
Sanjana Maheshwari <sanjana.maheshwari456@gmail.com>
2020-03-25 02:04:57 +0530

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,7 +3,7 @@
 # it's a great test of several basic programming patterns.
 
 for n in range(1, 101):
-    if n % 3 == 0 and n % 5 == 0:
+    if n % 3 == 0 and n % 5 == 0:       # n%15 == 0
         print('FizzBuzz')
     elif n % 3 == 0:
         print('Fizz')
