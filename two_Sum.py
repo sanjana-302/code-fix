14209916fa79a7a5791acd3e504c0d3098da24e6
Sanjana Maheshwari <sanjana.maheshwari456@gmail.com>
2020-04-14 21:15:06 +0530

    Update two_Sum.py

--- two_Sum.py
+++ two_Sum.py
@@ -5,4 +5,4 @@ target = 3
 for i in range(0, len(num), 1):             #complexity going wrong
     for j in range(i + 1, len(num), 1):
         if num[i] + num[j] == target:
-            print(f"{i} {j}")               #use return statement instead define a func
+            print(f"{i} {j}")               
