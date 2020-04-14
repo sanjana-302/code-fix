ec36282da3518b9c97c8a7fe687065d02431ed81
Sanjana Maheshwari <sanjana.maheshwari456@gmail.com>
2020-04-14 20:57:12 +0530

    logic

---
+++ two_Sum.py
@@ -0,0 +1,8 @@
+#logic
+num =[1,2,3,4]
+target = 3
+
+for i in range(0, len(num), 1):             #complexity going wrong
+    for j in range(i + 1, len(num), 1):
+        if num[i] + num[j] == target:
+            print(f"{i} {j}")               #use return statement instead
